c951d75e9fa3b98aa0f68229ac0386e8b6150bc3
ZheniaTrochun <zheniatrochun@gmail.com>
2018-03-02 18:10:14 +0200

    hotfix

--- src/main/scala/models/User.scala
+++ src/main/scala/models/User.scala
@@ -1,6 +1,15 @@
 package models
 
-case class UserCreds(val id: Option[Int] = None, val name: Option[String], val passwordHash: Option[String], val token: Option[String] = None)
+import com.byteslounge.slickrepo.meta.Entity
+
+case class UserCreds(val id: Option[Int] = None,
+                     val name: Option[String],
+                     val passwordHash: Option[String],
+                     val token: Option[String] = None)
+  extends Entity[UserCreds, Int] {
+
+  override def withId(id: Int) = this.copy(id = Some(id))
+}
 
 case class UserRegisterRequest(val name: String, val password: String, val email: String) {
   def toUserDto: UserDto = UserDto(name, password)
